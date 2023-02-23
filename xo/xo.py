230e98b538a00155c96ff952a6b915ded43a4609
barikirbis <126164085+barikirbis@users.noreply.github.com>
2023-02-23 20:09:12 +0300

    Update xo.py

--- xo/xo.py
+++ xo/xo.py
@@ -1,2 +1,30 @@
+cell_w = 100
+cell_margin = 10
+field_w = cell_w * 3 + cell_margin * 4 
+
+def cfg_canvas(width: int, height: int, canvas: Element) -> Context2d:
+    """
+    настройка канваса
+    """
+    ctx = canvas.getContext("2d")
+
+    canvas.style.width = f"{width}px"
+    canvas.style.height = f"{height}px"
+
+    canvas.width = width
+    canvas.height = height
+
+    ctx.clearRect(0, 0, width, height)
+
+    return ctx
+
+def draw_cell(x, y, w, h, ctx: Context2d):
+    """
+    рисуем одну клетку поля
+    """
+    ctx.fillRect(x, y, w, h)
+
 def main():
+    cfg_canvas(field_w, field_w, document.getElementById("canva"))
+    draw_cell(cell_margin, cell_margin, cell_w, cell_w, ctx)
     print("hello xo")
